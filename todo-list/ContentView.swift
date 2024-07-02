d58a1eacd6054a44fb7e90347edbe8cb20b0c4ac
Zachary Tumbusch <zach@w8zjt.net>
2024-07-02 08:13:26 -0400

    Initial Commit

---
+++ todo-list/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todo-list
+//
+//  Created by Zachary Tumbusch on 7/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todo-list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo-list
+//
+//  Created by Zachary Tumbusch on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
